c75277ef557f9bb007a69fbd81e302218a14f840
Denis Ouma <dennisouma38@gmail.com>
2023-09-12 11:27:18 +0300

    setting up the application repository and starter file

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello welcome Back");
+const logSomething = () => {
+    console.log("Hello Welcome");
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("Hello welcome Back");
+
+const logSomething = (): void => {
+	console.log("Hello Welcome");
+};
